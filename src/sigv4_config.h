738c5350922d957de523b83bc4446587676a6faf
Hiroaki Nakamura <hnakamur@gmail.com>
2023-01-11 22:38:07 +0900

    Delete ifdef __cplusplus from sigv4_config.h

--- src/sigv4_config.h
+++ src/sigv4_config.h
@@ -1,18 +1,4 @@
 #ifndef SIGV4_CONFIG_H_
 #define SIGV4_CONFIG_H_
 
-/* *INDENT-OFF* */
-#ifdef __cplusplus
-    extern "C" {
-#endif
-/* *INDENT-ON* */
-
-
-
-/* *INDENT-OFF* */
-#ifdef __cplusplus
-    }
-#endif
-/* *INDENT-ON* */
-
 #endif /* ifndef SIGV4_CONFIG_H_ */
