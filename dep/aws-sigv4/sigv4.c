16203506d665879e101eb53c2ea6515e9554d9e8
Hiroaki Nakamura <hnakamur@gmail.com>
2023-01-15 13:57:42 +0900

    Revert "Add debug logs in dep/aws-sigv4"
    This reverts commit 8c3a4e26ccb58dd4e8952b026ca87033556a3201.

--- dep/aws-sigv4/sigv4.c
+++ dep/aws-sigv4/sigv4.c
@@ -3228,10 +3228,6 @@ SigV4Status_t SigV4_GenerateHTTPAuthorization( const SigV4Parameters_t * pParams
         LogDebug( ( "Generated Canonical Request: %.*s",
                     ( unsigned int ) ( ( uint8_t * ) canonicalContext.pBufCur - canonicalContext.pBufProcessing ),
                     canonicalContext.pBufProcessing ) );
-        fprintf( stderr, "sigv4 Generated Canonical Request: %.*s!!!\n\n",
-                    ( unsigned int ) ( ( uint8_t * ) canonicalContext.pBufCur - canonicalContext.pBufProcessing ),
-                    canonicalContext.pBufProcessing );
-
 
         authPrefixLen = *authBufLen;
         returnStatus = generateAuthorizationValuePrefix( pParams,
@@ -3254,7 +3250,6 @@ SigV4Status_t SigV4_GenerateHTTPAuthorization( const SigV4Parameters_t * pParams
         hmacContext.pCryptoInterface = pParams->pCryptoInterface;
         signingKey.pData = canonicalContext.pBufCur;
         signingKey.dataLen = canonicalContext.bufRemaining;
-        fprintf(stderr, "sigv4 signingKey#1=%.*s!!!\n\n", (int) signingKey.dataLen, signingKey.pData);
         returnStatus = generateSigningKey( pParams,
                                            &hmacContext,
                                            &signingKey,
@@ -3266,7 +3261,6 @@ SigV4Status_t SigV4_GenerateHTTPAuthorization( const SigV4Parameters_t * pParams
     if( returnStatus == SigV4Success )
     {
         bufferLen = canonicalContext.pBufCur - ( char * ) canonicalContext.pBufProcessing;
-        fprintf(stderr, "sigv4 signingKey#2=%.*s!!!\n\n", (int) signingKey.dataLen, signingKey.pData);
         returnStatus = ( completeHmac( &hmacContext,
                                        signingKey.pData,
                                        signingKey.dataLen,
